9fb7b6ac4e5dc8105fcbb96ebe2ff6f302113269
GET MOUSSED <moussa.fof.pro@gmail.com>
2024-05-18 00:24:40 +0200

    ✨ added metadata display for current user

--- src/lib/components/player/screen.svelte
+++ src/lib/components/player/screen.svelte
@@ -6,33 +6,39 @@
   import UserActions from '$lib/components/user_actions.svelte';
   import { playlist } from '$lib/stores/playlist';
   import { is_mobile } from '$lib/stores/is_mobile';
+  import { App } from '$lib/app';
+  import { derived } from 'svelte/store';
 
-  let current_song: any | undefined = $playlist !== undefined ? $playlist[0] : undefined;
+  const current_meta = App.instance.context.audio_manager.readable('CURRENTLY_METADATA');
 
-  $: current_song = $playlist !== undefined ? $playlist[0] : undefined;
+  const image = derived(current_meta, $current_meta => {
+    if ($current_meta.img.length === 0) return '';
+    var blob = new Blob([new Uint8Array($current_meta.img[0].data)], { type: $current_meta.img[0].format });
+    return URL.createObjectURL(blob);
+  });
 </script>
 
 <div class={clsx('flex', 'flex-col', 'w-full', 'h-full', 'px-8', 'space-y-4', 'items-center', 'justify-center')}>
-  {#if current_song !== undefined}
-    <StyledCover alt="cover" src={current_song.cover} height="md:h-96" width="w-full md:w-96" />
+  <!-- {#if current_song !== undefined} -->
+  <StyledCover alt="cover" src={$image} height="md:h-96" width="w-full md:w-96" />
 
-    {#if $is_mobile}
-      <div class={clsx('flex', 'flex-col', 'w-full', 'relative')}>
-        <div class={clsx('space-y-2')}>
-          <h1 class={clsx('text-3xl', 'font-bold')}>
-            {current_song.title}
-          </h1>
-          <h4 class={clsx('text-sm')}>
-            {current_song.artist}
-          </h4>
-        </div>
-
-        <SongProgress />
-        <SongControls />
-        <UserActions />
+  {#if $is_mobile}
+    <div class={clsx('flex', 'flex-col', 'w-full', 'relative')}>
+      <div class={clsx('space-y-2')}>
+        <h1 class={clsx('text-3xl', 'font-bold')}>
+          {$current_meta.title}
+        </h1>
+        <h4 class={clsx('text-sm')}>
+          {$current_meta.artist}
+        </h4>
       </div>
-    {/if}
-  {:else}
-    <div>Pas de musique</div>
+
+      <SongProgress />
+      <SongControls />
+      <UserActions />
+    </div>
   {/if}
+  <!-- {:else}
+    <div>Pas de musique</div>
+  {/if} -->
 </div>
--- src/lib/notifier/audio_manager.ts
+++ src/lib/notifier/audio_manager.ts
@@ -3,146 +3,183 @@ import { Err, Ok, type Result } from 'bakutils-catcher';
 import { App } from '$lib/app';
 import { Notifier, type Events } from './i_notifier';
 import { SyncCurrentlyPlaying } from '$lib/commands/sync_currently_playing';
+import Mp3Tag from 'mp3tag.js';
+import type { MP3TagAPICFrame } from 'mp3tag.js/types/id3v2/frames';
 
-type AudioManagerEventType = 'CURRENTLY_PLAYING';
+type AudioManagerEventType = 'CURRENTLY_PLAYING' | 'CURRENTLY_METADATA';
 
 export type AudioManagerEvent = Events<
-  AudioManagerEventType,
-  {
-    CURRENTLY_PLAYING: {
-      type: 'CURRENTLY_PLAYING';
-      total_time: number;
-      current_time: number;
-      status: 'PLAYING' | 'PAUSED';
-    };
-  }
+	AudioManagerEventType,
+	{
+		CURRENTLY_PLAYING: {
+			type: 'CURRENTLY_PLAYING';
+			total_time: number;
+			current_time: number;
+			status: 'PLAYING' | 'PAUSED';
+		},
+		CURRENTLY_METADATA: {
+			type: 'CURRENTLY_METADATA';
+			title: string;
+			artist: string;
+			album: string;
+			year: string;
+			img: MP3TagAPICFrame[];
+		}
+	}
 >;
 
 export class AudioManager extends Notifier<AudioManagerEventType, AudioManagerEvent> {
-  private _context!: AudioContext;
-  private _audio = new Audio();
+	private _context!: AudioContext;
+	private _audio = new Audio();
 
-  /// both of those are used when it's you that is streaming the current song
-  private _node?: AudioBufferSourceNode;
-  private _destination?: MediaStreamAudioDestinationNode;
+	/// both of those are used when it's you that is streaming the current song
+	private _node?: AudioBufferSourceNode;
+	private _destination?: MediaStreamAudioDestinationNode;
 
-  /// this one is used to store the stream recived from host that the host rebroadcast to others
-  private _remote?: MediaStreamAudioSourceNode;
+	/// this one is used to store the stream recived from host that the host rebroadcast to others
+	private _remote?: MediaStreamAudioSourceNode;
 
-  private _current_time = 0;
-  private _media_timer: NodeJS.Timeout | undefined = undefined;
+	private _current_time = 0;
+	private _media_timer: NodeJS.Timeout | undefined = undefined;
 
-  /// getter to get the current stream playing either if its you that created it or got by a remote
-  get stream() {
-    return this._destination?.stream ?? this._remote?.mediaStream;
-  }
+	private _tag?: Omit<AudioManagerEvent['CURRENTLY_METADATA'], 'type'>;
 
-  try_init_audio_context() {
-    this._context ??= new AudioContext();
-  }
+	/// getter to get the current stream playing either if its you that created it or got by a remote
+	get stream() {
+		return this._destination?.stream ?? this._remote?.mediaStream;
+	}
 
-  constructor() {
-    super({
-      readable_default_values: {
-        CURRENTLY_PLAYING: {
-          type: 'CURRENTLY_PLAYING',
-          total_time: 0,
-          current_time: 0,
-          status: 'PAUSED',
-        },
-      },
-    });
-  }
+	try_init_audio_context() {
+		this._context ??= new AudioContext();
+	}
 
-  public async _prepareLocalStream(file: File): Promise<Result<null, Error>> {
-    const reader = new FileReader();
-    const buffer = new Completer<AudioBuffer>();
+	constructor() {
+		super({
+			readable_default_values: {
+				CURRENTLY_PLAYING: {
+					type: 'CURRENTLY_PLAYING',
+					total_time: 0,
+					current_time: 0,
+					status: 'PAUSED',
+				},
+				CURRENTLY_METADATA: {
+					type: 'CURRENTLY_METADATA',
+					title: '',
+					artist: '',
+					album: '',
+					year: '',
+					img: [],
+				},
+			},
+		});
+	}
 
-    reader.onload = event =>
-      this._context?.decodeAudioData(event.target?.result as ArrayBuffer, b => buffer.completeValue(b));
+	public async _prepareLocalStream(file: File): Promise<Result<null, Error>> {
+		const reader = new FileReader();
+		const buffer = new Completer<AudioBuffer>();
 
-    reader.readAsArrayBuffer(file);
-    this._node = this._context.createBufferSource();
-    const buf = await buffer.future;
-    if (buf.isNone()) return Err(Error('unable to create audio buffer from file'));
-    this._node.buffer = buf.unwrap();
-    this._destination = this._context.createMediaStreamDestination();
-    this._node.connect(this._context.destination);
-    this._node.connect(this._destination);
-    return Ok(null);
-  }
+		reader.onload = event => {
+			const b = event.target?.result as ArrayBuffer;
+			const tags = new Mp3Tag(b);
+			tags.read()
+			console.log(tags.tags);
+			this._tag = {
+				title: tags.tags.title!,
+				artist: tags.tags.artist!,
+				album: tags.tags.album!,
+				year: tags.tags.year!,
+				img: tags.tags.v2!.APIC!,
+				// img: tags.tags.v1Details,
+			};
+			this._notify({
+				type: 'CURRENTLY_METADATA',
+				...this._tag,
+			});
+			return this._context?.decodeAudioData(b, b => buffer.completeValue(b));
+		};
 
-  public stop() {
-    this._remote?.disconnect();
-    this._node?.stop();
-    this._node?.stop();
-    this._destination?.disconnect();
-    this._node = undefined;
-    this._destination = undefined;
-  }
+		reader.readAsArrayBuffer(file);
+		this._node = this._context.createBufferSource();
+		const buf = await buffer.future;
+		if (buf.isNone()) return Err(Error('unable to create audio buffer from file'));
+		this._node.buffer = buf.unwrap();
+		this._destination = this._context.createMediaStreamDestination();
+		this._node.connect(this._context.destination);
+		this._node.connect(this._destination);
+		return Ok(null);
+	}
 
-  public playRemote(stream: MediaStream) {
-    this._current_time = 0;
-    this.stop();
-    this._remote = this._context.createMediaStreamSource(stream);
-    this._remote.connect(this._context!.destination);
-    this._audio.srcObject = this._remote.mediaStream;
-  }
+	public stop() {
+		this._remote?.disconnect();
+		this._node?.stop();
+		this._node?.stop();
+		this._destination?.disconnect();
+		this._node = undefined;
+		this._destination = undefined;
+	}
 
-  public async resume() {
-    if (!this._node) return;
-    await this._context.resume();
-    const event: AudioManagerEvent['CURRENTLY_PLAYING'] = {
-      type: 'CURRENTLY_PLAYING',
-      total_time: this._node!.buffer!.duration,
-      current_time: this._current_time,
-      status: 'PLAYING',
-    };
-    App.instance.executeCommand(new SyncCurrentlyPlaying(event));
-    this._notify(event);
-    this._setupCurrentlyPlayingPeriodicPing();
-  }
+	public playRemote(stream: MediaStream) {
+		this._current_time = 0;
+		this.stop();
+		this._remote = this._context.createMediaStreamSource(stream);
+		this._remote.connect(this._context!.destination);
+		this._audio.srcObject = this._remote.mediaStream;
+	}
 
-  public async pause() {
-    if (!this._node) return;
+	public async resume() {
+		if (!this._node) return;
+		await this._context.resume();
+		const event: AudioManagerEvent['CURRENTLY_PLAYING'] = {
+			type: 'CURRENTLY_PLAYING',
+			total_time: this._node!.buffer!.duration,
+			current_time: this._current_time,
+			status: 'PLAYING',
+		};
+		App.instance.executeCommand(new SyncCurrentlyPlaying(event));
+		this._notify(event);
+		this._setupCurrentlyPlayingPeriodicPing();
+	}
 
-    await this._context.suspend();
-    const event: AudioManagerEvent['CURRENTLY_PLAYING'] = {
-      type: 'CURRENTLY_PLAYING',
-      total_time: this._node!.buffer!.duration,
-      current_time: this._current_time,
-      status: 'PAUSED',
-    };
-    App.instance.executeCommand(new SyncCurrentlyPlaying(event));
-    this._notify(event);
-    clearInterval(this._media_timer);
-  }
+	public async pause() {
+		if (!this._node) return;
 
-  private _setupCurrentlyPlayingPeriodicPing() {
-    clearInterval(this._media_timer);
-    this._media_timer = setInterval(() => {
-      const event: AudioManagerEvent['CURRENTLY_PLAYING'] = {
-        type: 'CURRENTLY_PLAYING',
-        total_time: this._node!.buffer!.duration,
-        current_time: ++this._current_time,
-        status: 'PLAYING',
-      };
-      this._notify(event);
-      App.instance.executeCommand(new SyncCurrentlyPlaying(event));
-    }, 1000);
+		await this._context.suspend();
+		const event: AudioManagerEvent['CURRENTLY_PLAYING'] = {
+			type: 'CURRENTLY_PLAYING',
+			total_time: this._node!.buffer!.duration,
+			current_time: this._current_time,
+			status: 'PAUSED',
+		};
+		App.instance.executeCommand(new SyncCurrentlyPlaying(event));
+		this._notify(event);
+		clearInterval(this._media_timer);
+	}
 
-    this._node!.onended = () => clearInterval(this._media_timer);
-  }
+	private _setupCurrentlyPlayingPeriodicPing() {
+		clearInterval(this._media_timer);
+		this._media_timer = setInterval(() => {
+			const event: AudioManagerEvent['CURRENTLY_PLAYING'] = {
+				type: 'CURRENTLY_PLAYING',
+				total_time: this._node!.buffer!.duration,
+				current_time: ++this._current_time,
+				status: 'PLAYING',
+			};
+			this._notify(event);
+			App.instance.executeCommand(new SyncCurrentlyPlaying(event));
+		}, 1000);
 
-  public async playLocal(file: File) {
-    this._current_time = 0;
-    this.stop();
+		this._node!.onended = () => clearInterval(this._media_timer);
+	}
 
-    await this._prepareLocalStream(file);
-    this._node!.start();
+	public async playLocal(file: File) {
+		this._current_time = 0;
+		this.stop();
 
-    this._setupCurrentlyPlayingPeriodicPing();
+		await this._prepareLocalStream(file);
+		this._node!.start();
 
-    return;
-  }
+		this._setupCurrentlyPlayingPeriodicPing();
+
+		return;
+	}
 }
