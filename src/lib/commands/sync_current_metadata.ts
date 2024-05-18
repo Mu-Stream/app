9582c4e794f531628bfa9071740335d4985ee3ea
GET MOUSSED <moussa.fof.pro@gmail.com>
2024-05-18 12:04:42 +0200

    ✨ sync cover and meta to other clients

--- src/lib/commands/sync_current_metadata.ts
+++ src/lib/commands/sync_current_metadata.ts
@@ -3,14 +3,15 @@ import { Command } from './i_commands';
 import type { CoreAppContext } from '$lib/app';
 import type { AudioManagerEvent } from '$lib/notifier/audio_manager';
 
-export class SyncCurrentlyPlaying extends Command<CoreAppContext> {
-	constructor(private event: AudioManagerEvent['CURRENTLY_PLAYING']) {
+export class SyncCurrentMetadata extends Command<CoreAppContext> {
+	constructor(private event: AudioManagerEvent['CURRENTLY_METADATA']) {
 		super();
 	}
 
 	public async execute(context: CoreAppContext): Promise<Result<null, Error>> {
 		context.room.send(this.event);
 		context.room.broadcast(this.event);
+		context.audio_manager.syncCurrentMetadata(this.event);
 		return Ok(null);
 	}
 }
