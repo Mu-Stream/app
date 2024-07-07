1311484e426234f22a3492b9b5480f7bf1943453
GET_MOUSSED <mofanafoussa@gmail.com>
2024-07-07 22:54:15 +0200

    added logo and tutorials

--- src/lib/components/user_actions.svelte
+++ src/lib/components/user_actions.svelte
@@ -19,7 +19,7 @@
   class={clsx('flex', 'items-center', 'justify-center md:justify-end', 'py-4 md:py-0', 'space-x-2', 'px-4')}
   bind:this={App.instance.plugin_manager.user_actions_shortcut_ref}
 >
-  <button class={clsx('btn', 'variant-filled-tertiary', outline_style)} on:click={pick_songs}>
+  <button id="btn-add-song" class={clsx('btn', 'variant-filled-tertiary', outline_style)} on:click={pick_songs}>
     <FileMusicSolid />
     <span class="hidden md:inline">ajouter un son</span>
   </button>
