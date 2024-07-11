818bc0d2f3533f4982ca613075a2a3df96112b25
GET_MOUSSED <mofanafoussa@gmail.com>
2024-07-11 20:13:07 +0200

    fix cover parsing

--- src/lib/plugins/playlist/commands/add_to_playlist.ts
+++ src/lib/plugins/playlist/commands/add_to_playlist.ts
@@ -9,43 +9,48 @@ import { parseBlob } from 'music-metadata';
 import { v4 } from 'uuid';
 
 export class AddToPlaylist extends Command<PlaylistPlugin['context']> {
-	constructor(private _file: File) {
-		super();
-	}
+  constructor(private _file: File) {
+    super();
+  }
 
-	public async execute(context: PlaylistPlugin['context']): Promise<Result<null, Error>> {
+  public async execute(context: PlaylistPlugin['context']): Promise<Result<null, Error>> {
+    let tags: {
+      common: {
+        title?: string;
+        artist?: string;
+        album?: string;
+        year?: number;
+        picture?: { data: Uint8Array; format: string }[];
+      };
+    };
 
-		let tags: {
-			common: { title?: string; artist?: string; album?: string; year?: number; picture?: { data: Uint8Array, format: string }[] };
-		};
+    try {
+      tags = await parseBlob(this._file);
+    } catch (e) {
+      tags = { common: {} };
+    }
 
-		try {
-			tags = await parseBlob(this._file);
-		} catch (e) {
-			tags = { common: {} };
-		}
+    const met: Song = {
+      identity: context.room.client_peer?.id ?? 'host',
+      uuid: v4(),
+      title: tags.common.title ?? this._file.name.split('.')[0],
+      artist: tags.common.artist ?? '',
+      album: tags.common.album ?? '',
+      year: tags.common.year?.toString() ?? '',
+      img: tags.common.picture ?? [],
+    };
 
-		const met: Song = {
-			identity: context.room.client_peer?.id ?? 'host',
-			uuid: v4(),
-			title: tags.common.title ?? this._file.name.split('.')[0],
-			artist: tags.common.artist ?? '',
-			album: tags.common.album ?? '',
-			year: tags.common.year?.toString() ?? '',
-			img: tags.common.picture ?? [],
-		}
+    // compress image  to make it go through webrtc and keep only the first one
+    if (met.img.length !== 0) {
+      var blob = new Blob([met.img[0].data], { type: met.img[0].format });
+      const compressed = await imageCompression(new File([blob], 'temp', { type: met.img[0].format }), {
+        maxSizeMB: get(compression_rate),
+      });
+      met.img[0].data = new Uint8Array(await compressed.arrayBuffer());
+      met.img = met.img.slice(0, 1);
+    }
 
-		// compress image  to make it go through webrtc and keep only the first one
-		if (met.img.length !== 0) {
-			var blob = new Blob([new Uint8Array(met.img[0].data)], { type: met.img[0].format });
-			const compressed = await imageCompression(new File([blob], 'temp', { type: met.img[0].format }), {
-				maxSizeMB: get(compression_rate),
-			});
-			met.img[0].data = new Uint8Array(await compressed.arrayBuffer());
-			met.img = met.img.slice(0, 1);
-		}
-
-		context.playlist.playlist_manager.addSongToPlaylist(met, this._file);
-		return Ok(null);
-	}
+    context.playlist.playlist_manager.addSongToPlaylist(met, this._file);
+    return Ok(null);
+  }
 }
