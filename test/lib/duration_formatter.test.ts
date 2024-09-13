42bc15d3c38e7a3932144f3402f728ffca7ea08b
Ebbane D <diet.ebbane@gmail.com>
2024-09-14 00:02:43 +0200

    feat : add some test

---
+++ test/lib/duration_formatter.test.ts
@@ -0,0 +1,15 @@
+import {describe, expect, it} from 'vitest';
+import {formatSeconds} from '../../src/lib/duration_formatter';
+
+describe('formatSeconds', () => {
+  it.each([
+    [0, '0:00'],
+    [59, '0:59'],
+    [60, '1:00'],
+    [61, '1:01'],
+    [3599, '59:59'],
+    [3600, '60:00']
+  ])('should format %i seconds as "%s"', (input, expected) => {
+    expect(formatSeconds(input)).toBe(expected);
+  });
+});
