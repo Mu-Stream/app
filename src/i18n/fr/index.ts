38f06d508cdfbd3df23bc08e01f009b5960629ac
Ebbane D <diet.ebbane@gmail.com>
2024-09-14 14:14:18 +0200

    fix : français

--- src/i18n/fr/index.ts
+++ src/i18n/fr/index.ts
@@ -1,4 +1,4 @@
-import type { Translation } from '../i18n-types';
+import type {Translation} from '../i18n-types';
 
 const fr = {
   title: 'Mu Stream',
@@ -18,25 +18,25 @@ const fr = {
     stepWelcome: {
       title: 'Bienvenue sur Mu Stream',
       description:
-        "Mu Stream est une application qui te permet de partager des sessions d'écoute avec tes amis. Voici un tuto pour t'aider à démarrer.",
+          "Mu Stream est une application qui te permet de partager des sessions d'écoute avec tes amis. Voici un tuto pour t'aider à démarrer.",
     },
     stepCreateRoom: {
-      title: 'Créez une salle',
+      title: 'Créer une salle',
       description:
-        "Clique ici pour créer une salle. Un code sera créé, tu pourras le copier et le partager avec tes amis pour qu'ils te rejoignent.",
+          "Clique ici pour créer une salle. Un code sera créé, tu pourras le copier et le partager avec tes amis pour qu'ils te rejoignent.",
     },
     stepJoinRoom: {
       title: 'Rejoindre une salle',
       description:
-        "Clique ici pour créer une salle. Un code de salle seras créer, tu pourras le copier et le partager avec tes amis pour qu'ils puissent te rejoindre.",
-      description1: 'Cliquer ici pour rejoindre une salle.',
-      description2: "Renseigner ton nom d'utilisateur ici.",
-      description3: 'Renseigner le code de la salle ici.',
-      description4: "Est c'est parti !",
+          "Clique ici pour rejoindre une salle. Un code de salle sera créé, tu pourras le copier et le partager avec tes amis pour qu'ils puissent te rejoindre.",
+      description1: 'Clique ici pour rejoindre une salle.',
+      description2: "Renseigne ton nom d'utilisateur ici.",
+      description3: 'Renseigne le code de la salle ici.',
+      description4: "Et c'est parti !",
     },
   },
   roomScreen: {
-    code: 'code',
+    code: 'Code',
     deleteRoom: 'Supprimer la salle',
     logOut: 'Déconnexion',
     participants: 'Participants',
@@ -46,18 +46,18 @@ const fr = {
   },
   warningPopup: {
     description:
-      "Mu Stream n'est pas responsable du contenu audio diffusé par les participants. Vous en êtes seul responsable.",
+        "Mu Stream n'est pas responsable du contenu audio diffusé par les participants. Vous en êtes seul responsable.",
     quit: 'Ne plus afficher',
   },
   roomScreenTutorial: {
     stepWelcome: {
       title: 'Bienvenue dans cette salle',
-      description: "Tu es dans une salle d`'écoute privé de musique. Voici un tuto pour t'aider à naviguer.",
+      description: "Tu es dans une salle d'écoute privée de musique. Voici un tuto pour t'aider à naviguer.",
     },
     stepCode: {
       title: 'Code de la salle',
       description:
-        "Tu peux partager ce code avec tes amis pour qu'ils rejoignent ta salle.<br>L'œil permet de l'afficher et l'icône copier de le copier directement dans ton presse-papier.",
+          "Tu peux partager ce code avec tes amis pour qu'ils rejoignent ta salle.<br>L'œil permet de l'afficher et l'icône copier de le copier directement dans ton presse-papier.",
     },
     stepAddSong: {
       title: 'Ajouter un son',
@@ -65,7 +65,7 @@ const fr = {
     },
     stepReact: {
       title: 'Ajouter une réaction',
-      description: 'Tu peux reagir a la musique !',
+      description: 'Tu peux réagir à la musique !',
     },
     stepMusicControl: {
       title: 'Contrôles de la musique',
@@ -81,12 +81,12 @@ const fr = {
     },
   },
   common: {
-    tutorialStep: 'Etape {current} sur {total}',
+    tutorialStep: 'Étape {current} sur {total}',
   },
   alertToast: {
     newParticipant: '{name} a rejoint la salle',
     participantLeft: '{name} a quitté la salle',
-    copiedRoomCode: 'Code copié dans le presse papier',
+    copiedRoomCode: 'Code copié dans le presse-papier',
   },
   settingsPopup: {
     language: 'Langue',
